fce3ce53944faac492b434b1fad52016ba62cf42
leandroschepers <leandrocompolt@gmail.com>
2020-01-24 11:48:56 -0300

    Mapa

--- app/src/main/java/com/example/debarembar/MainActivity.java
+++ app/src/main/java/com/example/debarembar/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.debarembar;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnOpen = findViewById(R.id.openMap);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "• Iniciando mapa •", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(v.getContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
