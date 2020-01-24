3280d25758d2147eab79f466d9a9c3d6f5f45687
= <=>
2020-01-24 11:41:22 -0300

    contatos

--- app/src/main/java/com/example/debarembar/Contatos.java
+++ app/src/main/java/com/example/debarembar/Contatos.java
@@ -1,42 +1,104 @@
 package com.example.debarembar;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class Contatos extends AppCompatActivity {
 
-    private static final int RESULT_PICK_CONTACT =1;
+    private static final int RESULT_PICK_CONTACT = 1;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 123 ;
     private TextView phone;
     private Button select;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    Button sendBtn;
+    EditText txtphoneNo;
+    EditText txtMessage;
+    String phoneNo;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_contatos);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contatos);
 
-        phone = findViewById (R.id.phone);
-        select = findViewById (R.id.select);
+        phone = findViewById(R.id.phone);
+        select = findViewById(R.id.select);
 
-        select.setOnClickListener (new View.OnClickListener () {
+        select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in = new Intent (Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult (in, RESULT_PICK_CONTACT);
+                Intent in = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(in, RESULT_PICK_CONTACT);
+            }
+        });
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+        txtphoneNo = (EditText) findViewById(R.id.editText);
+        txtMessage = (EditText) findViewById(R.id.editText2);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
             }
         });
 
     }
+    protected void sendSMSMessage() {
+        Log.i("Escreve ai", "");
+        String phoneNo = txtphoneNo.getText().toString();
+        String message = txtMessage.getText().toString();
 
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "Foi.", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Deu ruim irmao.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void onActivityResult (int requestCode, int resultCode,  Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -70,4 +132,5 @@ public class Contatos extends AppCompatActivity {
             e.printStackTrace ();
         }
     }
+
 }
